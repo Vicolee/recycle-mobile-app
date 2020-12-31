184a10c3c09ed96fd9992fd20fa1200981c8953f
scharfsteina <ava.l.scharfstein.24@dartmouth.edu>
2020-12-30 20:30:06 -0700

    first commit

--- recycle_app/lib/how_to_use.dart
+++ recycle_app/lib/how_to_use.dart
@@ -1,20 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+import 'package:recycle_app/camera.dart';
+import 'package:recycle_app/how_to_use.dart';
+import 'package:recycle_app/info.dart';
+import 'package:recycle_app/pgraph.dart';
 
 class HowToUse extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("How To Use"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Back to Home Page'),
+        appBar: AppBar(
+          title: Text(
+            "How To Use",
+          ), // style: TextStyle(color: Color.fromRGBO(0, 0, 46, 1))
+          backgroundColor: Color.fromRGBO(115, 168, 232, 1),
         ),
-      ),
-    );
+        body: Container(
+          margin: new EdgeInsets.only(top: 40, left: 18, right: 18),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Center(
+                child: Text(
+                  'Recycling! is comprised of three pages.',
+                  style: TextStyle(
+                    color: Color.fromRGBO(0, 0, 46, 1),
+                    fontSize: 19,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Text('\n'),
+
+              // Information about Camera page
+              // the word 'Camera' is linked to the Camera page (see Navigator.push...)
+              // 'Camera' also is enlarged
+              PGraph(
+                link: 'Camera:',
+                paragraph:
+                    ' After snapping an image of waste to be discarded, our image recognition software will evaluate the photo and bring you to a page with detailed information on what to do with your waste.',
+              ),
+
+              Text('\n'),
+
+              // Information about How To Recycle page
+              // the phrase 'How To Recycle' is linked to the Info page (see Navigator.push...)
+              // 'How To Recycle' also is enlarged
+              PGraph(
+                  link: 'How to Recycle:',
+                  paragraph:
+                      ' The goal of this page is to provide you with substantial information about recycling, such that you will be equipped with the knowledge to recycle correctly in the future.'),
+
+              Text('\n'),
+
+              // // Information about How to Use page
+              // the phrase 'How to Use' is linked to the How to Use page (see Navigator.push...)
+              // 'How to Use' also is enlarged
+              PGraph(
+                  link: 'How To Use:',
+                  paragraph:
+                      ' The page you are currently on that tells you how to utilize the features of Recycling! most optimally.'),
+
+              Text('\n'),
+
+              // TODO: COULD CHANGE THIS BUTTON INTO LOGO THAT GOES BACK TO HOME PAGE
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Back to Home Page'),
+              )
+            ],
+          ),
+        ));
   }
 }
---
+++ recycle_app/lib/pgraph.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+import 'package:recycle_app/camera.dart';
+import 'package:recycle_app/how_to_use.dart';
+import 'package:recycle_app/info.dart';
+
+class PGraph extends StatelessWidget {
+  // word to be linked
+  final String link;
+  // regular paragraph
+  final String paragraph;
+
+  PGraph({this.link, this.paragraph});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: RichText(
+      text: TextSpan(
+        children: <TextSpan>[
+          TextSpan(
+              text: link,
+              style: TextStyle(
+                  // make linked word larger and a light blue
+                  color: Color.fromRGBO(115, 168, 232, 1),
+                  fontSize: 30),
+              recognizer: new TapGestureRecognizer()
+                ..onTap = () {
+                  // if link is camera, set link to Camera class
+                  if (link == 'Camera:') {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Camera()),
+                    );
+                    // if link is info, set link to Info class
+                  } else if (link == 'How to Recycle:') {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Info()),
+                    );
+                    // else link is how to use, don't do anything
+                    // because it should go to the same page
+                  } else {
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(builder: (context) => HowToUse()),
+                    // );
+                    // DO NOTHING
+                  }
+                }),
+          TextSpan(text: paragraph),
+        ],
+        style: TextStyle(color: Color.fromRGBO(0, 0, 46, 1), fontSize: 20),
+      ),
+      textAlign: TextAlign.center,
+    ));
+  }
+}
