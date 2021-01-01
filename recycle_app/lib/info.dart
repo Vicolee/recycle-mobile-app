0d9bc4f5cd50e189b120eb245cf428b77283b7aa
lizfrey <frey34145@sas.edu.sg>
2021-01-02 00:48:19 +0800

    initial commit, added scrollable how to use page

--- recycle_app/lib/info.dart
+++ recycle_app/lib/info.dart
@@ -5,16 +5,71 @@ class Info extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Info Page"),
+        title: Text(
+          "How To Use",
+        ), // style: TextStyle(color: Color.fromRGBO(0, 0, 46, 1))
+        backgroundColor: Color.fromRGBO(115, 168, 232, 1),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Back to Home Page'),
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.vertical, //.horizontal
+                  padding: EdgeInsets.all(20),
+                  child: Text(
+                    "1 Description that is too long in text format(Here Data is coming from API) jdlksaf j klkjjflkdsjfkddfdfsdfds " +
+                        "2 Description that is too long in text format(Here Data is coming from API) d fsdfdsfsdfd dfdsfdsf sdfdsfsd d " +
+                        "3 Description that is too long in text format(Here Data is coming from API)  adfsfdsfdfsdfdsf   dsf dfd fds fs" +
+                        "4 Description that is too long in text format(Here Data is coming from API) dsaf dsafdfdfsd dfdsfsda fdas dsad" +
+                        "5 Description that is too long in text format(Here Data is coming from API) dsfdsfd fdsfds fds fdsf dsfds fds " +
+                        "6 Description that is too long in text format(Here Data is coming from API) asdfsdfdsf fsdf sdfsdfdsf sd dfdsf" +
+                        "7 Description that is too long in text format(Here Data is coming from API) df dsfdsfdsfdsfds df dsfds fds fsd" +
+                        "8 Description that is too long in text format(Here Data is coming from API)" +
+                        "9 Description that is too long in text format(Here Data is coming from API)" +
+                        "10 Description that is too long in text format(Here Data is coming from API)",
+                    style: new TextStyle(
+                      color: Color.fromRGBO(0, 0, 46, 1),
+                      fontSize: 19,
+                    ),
+                  ),
+                ),
+              ),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Back to Home Page'),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
+  /* Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Recycling Information'),
+        backgroundColor: Color.fromRGBO(115, 168, 232, 1),
+      ),
+      body: Container(
+          margin: new EdgeInsets.only(top: 40, left: 18, right: 18),
+          
+          /* Column(children: <Widget>[
+          Text(''),
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Back to Home Page'),
+            ),
+          ),
+        ]), */
+          ),
+    ); */
 }
